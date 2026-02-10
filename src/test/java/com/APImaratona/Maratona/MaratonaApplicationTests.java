7a6adca192e6ca60ab094de96d9d4ca48f6914bd
Arthur <you@example.com>
2026-02-10 17:40:43 -0300

    priemiro commmit

---
+++ src/test/java/com/APImaratona/Maratona/MaratonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APImaratona.Maratona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaratonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
