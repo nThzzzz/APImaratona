f958b7e2f0c4ae7e4c82d0ea548fa70c7192771e
Arthur <you@example.com>
2026-02-10 18:19:55 -0300

    feat: conexao com o banco de dados

--- src/main/java/com/APImaratona/Maratona/MaratonaApplication.java
+++ src/main/java/com/APImaratona/Maratona/MaratonaApplication.java
@@ -3,6 +3,8 @@ package com.APImaratona.Maratona;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class MaratonaApplication {
 
