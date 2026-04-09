bf1e380f0febcea6ef69d32c34d3170b5672e59f
Arthur <you@example.com>
2026-04-09 00:10:13 -0300

    chore: adiciona TODO para implementacao futura de manipulador global de excecoes

--- src/main/java/com/APImaratona/Maratona/MaratonaApplication.java
+++ src/main/java/com/APImaratona/Maratona/MaratonaApplication.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 @SpringBootApplication
 public class MaratonaApplication {
 
+    //TODO: DEPOIS DE FUNCIONAL REFATORAR AS EXECPTIONS PARA RETORNAR O STATUS DA APLICACAO
 	public static void main(String[] args) {
 		SpringApplication.run(MaratonaApplication.class, args);
 	}
